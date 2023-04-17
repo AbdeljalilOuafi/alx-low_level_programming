7ee28341878316d91e1b8334970ad713d0287051
AbdeljalilOuafi <abdeljalilouafi55@gmail.com>
2023-04-17 15:08:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+#endif
