77025e9bdac31ebd7a281bc1f87f19b542d9cf3d
AbdeljalilOuafi <abdeljalilouafi55@gmail.com>
2023-04-20 15:22:33 -0700

    sum them all fixed betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters.
- * @n : parameters 
+ * @n : parameters
  * Describtion : Made by Abdeljalil Ouafi
  * Return: sum and if n equal zero return it
  *
