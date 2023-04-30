6fdac5089624805a1aca203bbbe1cbc1e3dee2f1
Abdeljalil Ouafi <abdeljalilouafi55@gmail.com>
2023-04-30 11:35:08 -0700

    betty style fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h> 
+#include <stddef.h>
 #include <string.h>
 
 /**
@@ -11,12 +11,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
