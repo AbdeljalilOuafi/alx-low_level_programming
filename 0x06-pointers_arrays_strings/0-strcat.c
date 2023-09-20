6f2e0f539e88417ce8f27c7f6626d336784ddded
AbdeljalilOuafi <abdeljalilouafi44@gmail.com>
2023-09-20 12:08:30 +0100

    betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,12 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: char
+ * @src: char
+ * Return: char
+*/
 char *_strcat(char *dest, char *src)
 {
-	char * s = dest;
+	char *s = dest;
 
 	while (*dest != '\0')
 	{
