96791feb04c30c60b7a0eae12932f1b2f6060beb
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2023-10-02 12:24:09 +0100

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: Alwyas 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
