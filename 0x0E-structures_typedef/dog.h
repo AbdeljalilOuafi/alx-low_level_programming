4bd481bc6cf344e52381ebd22d6c3d994f1fa1f4
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2023-10-10 20:38:26 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef __DOG__
+#define __DOG__
+
+/**
+ * struct dog - structure for a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: No description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* __DOG__ */
